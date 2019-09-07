05f5c84d74bccac09fcbe7542cf8d59b5d959a34
Andrew Jones <ascjones@gmail.com>
2019-09-08 00:51:05 +0100

    Cargo fmt

--- src/exchanges/coinbase.rs
+++ src/exchanges/coinbase.rs
@@ -13,57 +13,57 @@ use crate::trades::{Trade, TradeKind};
 #[derive(Debug, Deserialize, Clone)]
 #[allow(non_snake_case)]
 struct Record {
-	#[serde(rename = "trade id")]
-	trade_id: String,
-	product: String,
-	side: String,
-	#[serde(rename = "created at")]
-	created_at: String,
-	size: f64,
-	#[serde(rename = "size unit")]
-	size_unit: String,
-	price: f64,
-	fee: f64,
-	total: f64,
-	#[serde(rename = "price/fee/total unit")]
-	unit: String,
+    #[serde(rename = "trade id")]
+    trade_id: String,
+    product: String,
+    side: String,
+    #[serde(rename = "created at")]
+    created_at: String,
+    size: f64,
+    #[serde(rename = "size unit")]
+    size_unit: String,
+    price: f64,
+    fee: f64,
+    total: f64,
+    #[serde(rename = "price/fee/total unit")]
+    unit: String,
 }
 
 impl Into<Option<Trade>> for Record {
-	fn into(self) -> Option<Trade> {
-		// 2018-11-20T21:39:45.667Z
-		let date_time =
-			NaiveDateTime::parse_from_str(self.created_at.as_ref(), "%Y-%m-%dT%H:%M:%S%z").unwrap();
+    fn into(self) -> Option<Trade> {
+        // 2018-11-20T21:39:45.667Z
+        let date_time =
+            NaiveDateTime::parse_from_str(self.created_at.as_ref(), "%Y-%m-%dT%H:%M:%S%z").unwrap();
 
-		let mut market_parts = self.product.split('-');
-		let quote_currency = market_parts.next().expect("quote currency");
-		let base_currency = market_parts.next().expect("base currency");
+        let mut market_parts = self.product.split('-');
+        let quote_currency = market_parts.next().expect("quote currency");
+        let base_currency = market_parts.next().expect("base currency");
 
-		let base_amount = amount(base_currency, self.size);
-		let quote_amount = amount(quote_currency, self.total);
+        let base_amount = amount(base_currency, self.size);
+        let quote_amount = amount(quote_currency, self.total);
 
-		let (kind, sell, buy) = match self.side.as_ref() {
-			"BUY" => (TradeKind::Buy, quote_amount, base_amount),
-			"SELL" => (TradeKind::Sell, base_amount, quote_amount),
-			_ => panic!("Invalid order_type {}", self.side),
-		};
-		let fee = amount(self.unit.as_ref(), self.fee);
+        let (kind, sell, buy) = match self.side.as_ref() {
+            "BUY" => (TradeKind::Buy, quote_amount, base_amount),
+            "SELL" => (TradeKind::Sell, base_amount, quote_amount),
+            _ => panic!("Invalid order_type {}", self.side),
+        };
+        let fee = amount(self.unit.as_ref(), self.fee);
 
-		Some(Trade {
-			date_time,
-			kind,
-			buy,
-			sell,
-			fee,
-			rate: self.price,
-			exchange: Some("Coinbase Pro".into()),
-		})
-	}
+        Some(Trade {
+            date_time,
+            kind,
+            buy,
+            sell,
+            fee,
+            rate: self.price,
+            exchange: Some("Coinbase Pro".into()),
+        })
+    }
 }
 
 pub fn import_trades<R>(reader: R) -> Result<Vec<Trade>, Box<dyn Error>>
-	where
-		R: Read,
+where
+    R: Read,
 {
-	super::csv_to_trades::<R, Record>(reader)
+    super::csv_to_trades::<R, Record>(reader)
 }
